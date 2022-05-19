7e32243d1e0f7bcdaf9b159d92d8803d60baa5c8
Mylene Febrissy <m.febrissy@gmail.com>
2022-05-19 12:19:17 +0200

    Models ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,11 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :list, presence: true
+  validates :movie, presence: true
+
+  validates :comment, length: { minimum: 6 }
+
+  validates_uniqueness_of :movie_id, scope: [:list_id]
 end
